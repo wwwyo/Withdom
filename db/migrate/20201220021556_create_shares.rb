4ed07f12d39901cd28bedfca04356b51c20de7a5
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-12-20 11:42:37 +0900

    Add: database

---
+++ db/migrate/20201220021556_create_shares.rb
@@ -0,0 +1,12 @@
+class CreateShares < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shares do |t|
+      t.string :title, null: false
+      t.string :share_url, null: false
+      t.string :comment, null: false
+      t.integer :category_id, null: false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
