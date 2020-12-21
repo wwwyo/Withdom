9dc3979b9c3bc79d882e8af1894da19367723b66
hoya-syta <erukhnjhgfd@gmail.com>
2020-12-21 17:22:37 +0900

    add:編集・投稿ページの成形

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,2 +1,10 @@
 class Share < ApplicationRecord
+  validates :share_url,   presence: true
+  validates :title,       presence: true
+  validates :comment,     presence: true
+  validates :category_id, presence: true
+
+
+  belongs_to :user
+
 end
