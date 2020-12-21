946e1a1021392eea07354b1d0d7caeb741a2d498
KatsujiOgawa <marug711@gmail.com>
2020-12-21 15:03:28 +0900

    update: エラーメッセージによるレイアウト崩れを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'は英数混合', allow_blank: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  
+  has_many :shares
+
+
 end
