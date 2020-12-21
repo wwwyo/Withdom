2a7c61a93cddee57adb94582986d6bd6649db25a
hoya-syta <erukhnjhgfd@gmail.com>
2020-12-21 17:49:56 +0900

    add:コントローラー編集

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,10 +1,34 @@
 class SharesController < ApplicationController
+  before_action :set_item, only: [:edit, :update, :destroy]
+
   def index
   end
   
-  def new
+  def new 
+    @share = Share.new
+  end
+
+  def create
+    Share.create(share_params)
   end
 
   def edit
   end
+
+  def update
+    @share.update(share_params)
+  end
+
+  def destroy
+    @share.destroy
+  end
+
+
+  private
+  def set_item
+    @share = Share.find(params[:id])
+  end
+
+  def  share_params
+    params.require(:share).permit(:share_url, :text).merge(user_id: current_user.id)
 end
