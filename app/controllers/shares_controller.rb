77eecd0d9727772b1e695606cd4a6fca270767f8
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-12-21 20:44:05 +0900

    Add: indexアクション

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,5 +1,13 @@
 class SharesController < ApplicationController
   def index
+    @categories = Category.all
+    if params[:id]
+      @shares = Share.includes(:user).where(category_id: params[:id]).order(created_at: :desc)
+      @path_id = request.fullpath.split("/shares?id=").last
+    else
+      @shares = Share.includes(:user).order(created_at: :desc)
+      @path_id = nil
+    end
   end
   
   def new
