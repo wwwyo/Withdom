1d0bf099db80ced4b4cb5e696ec83239d3aaf18c
ksk-k <kitaichikosuke0203@gmail.com>
2020-12-27 00:35:26 +0900

    add:新規投稿単一テスト

--- spec/factories/shares.rb
+++ spec/factories/shares.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :share do
-    
+    id {1}
+    title {Faker::Name.initials(number: 2)}
+    share_url {"https://qiita.com/"}
+    comment {Faker::Name.initials(number: 2)}
+    category_id {1}
+    user_id {1}
+    user
   end
 end
--- spec/models/share_spec.rb
+++ spec/models/share_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
-
 RSpec.describe Share, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @share = FactoryBot.build(:share)
+  end
+
+  describe "新規投稿" do
+    context "新規投稿がうまくいくとき" do
+      it "title,share_url,comment,category_id,user_idが存在すれば登録できる" do
+        expect(@share).to be_valid
+      end
+    end
+    context "新規投稿が上手く行かないとき" do
+      it "titleが空だと登録できない" do
+        @share.title = ""
+        @share.valid?
+        expect(@share.errors.full_messages).to include("Titleを入力してください")
+      end
+      it "share_urlが空だと登録できない" do
+        @share.share_url = ""
+        @share.valid?
+        expect(@share.errors.full_messages).to include("URLを入力してください")
+      end
+      it "commentが空だと登録できない" do
+        @share.comment = ""
+        @share.valid?
+        expect(@share.errors.full_messages).to include("Commentを入力してください")
+      end
+      it "category_idが空だと登録できない" do
+        @share.category_id = ""
+        @share.valid?
+        expect(@share.errors.full_messages).to include("Categoryを入力してください")
+      end
+    end
+  end
 end
