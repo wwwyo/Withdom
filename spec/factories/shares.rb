357cd1ab95040c2f63a9cb0de33cbaffb9764fcf
ksk-k <kitaichikosuke0203@gmail.com>
2020-12-27 15:19:20 +0900

    コメントに対する修正
    idの削除、association :userに修正

--- spec/factories/shares.rb
+++ spec/factories/shares.rb
@@ -1,11 +1,9 @@
 FactoryBot.define do
   factory :share do
-    id {1}
     title {Faker::Name.initials(number: 2)}
     share_url {"https://qiita.com/"}
     comment {Faker::Name.initials(number: 2)}
     category_id {1}
-    user_id {1}
-    user
+    association :user
   end
 end
